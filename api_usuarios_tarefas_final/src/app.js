19f2f7847c886dbbfaabcc82a62aee5bdca04310
xxlufi4ggxx <leo25nogueira@gmail.com>
2025-10-21 20:39:06 -0300

    Update app.js

--- api_usuarios_tarefas_final/src/app.js
+++ api_usuarios_tarefas_final/src/app.js
@@ -7,4 +7,4 @@ app.use(express.json());
 app.use('/users', userRoutes);
 app.use('/tasks', taskRoutes);
 
-app.listen(3333, () => console.log('🚀 Servidor rodando na porta 3333'));
+app.listen(3333, () => console.log(' Servidor rodando na porta 3333'));
